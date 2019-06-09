00c4be7d8d0c66d0ed7b639f607eb08201dc3d65
Anton Gepting <anton.gepting@gmail.com>
2019-06-09 02:02:06 +0200

    feat: add show-options function

--- src/options.rs
+++ src/options.rs
@@ -1,8 +1,57 @@
 use super::tmux_interface::*;
+use super::tmux_interface_error::TmuxInterfaceError;
+use std::borrow::Cow;
+
+
+/// Show options structure
+///
+/// # Manual
+///
+/// ```text
+/// tmux show-options [-gHqsvw] [-t target-session | target-window] [option]
+/// (alias: show)
+/// ```
+pub struct ShowOptions<'a> {
+    /// global session or window options are listed
+    pub global_options: Option<bool>,           // [-g]
+    /// includes hooks (omitted by default)
+    pub hooks: Option<bool>,                    // [-H]
+    /// no error will be returned if `option` is unset
+    pub quiet: Option<bool>,                    // [-q]
+    /// show the server options
+    pub server_options: Option<bool>,           // [-s]
+    /// shows only the option value
+    pub option_value: Option<bool>,             // [-v]
+    /// show the window options
+    pub window_options: Option<bool>,           // [-w]
+    /// target session or window name
+    pub target: Option<Cow<'a, str>>,           // [-t target-session | target-window]
+    /// option name
+    pub option: Option<Cow<'a, str>>,           // [option]
+}
+
+
+impl<'a> Default for ShowOptions<'a> {
+    fn default() -> Self {
+        ShowOptions {
+            global_options: None,
+            hooks: None,
+            quiet: None,
+            server_options: None,
+            option_value: None,
+            window_options: None,
+            target: None,
+            option: None
+        }
+    }
+}
 
 
 /// Options
 impl<'a> TmuxInterface<'a> {
+
+
+    const SHOW_OPTIONS: &'static str = "show-options";
 
 
     /// # Manual
@@ -33,7 +82,18 @@ impl<'a> TmuxInterface<'a> {
     /// tmux show-options [-gHqsvw] [-t target-session | target-window] [option]
     /// (alias: show)
     /// ```
-    pub fn show_options() {
+    pub fn show_options(&self, show_options: &ShowOptions) -> Result<bool, TmuxInterfaceError> {
+        let mut args: Vec<&str> = Vec::new();
+        if show_options.global_options.unwrap_or(false) { args.push(g_KEY); }
+        if show_options.hooks.unwrap_or(false) { args.push(H_KEY); }
+        if show_options.quiet.unwrap_or(false) { args.push(q_KEY); }
+        if show_options.server_options.unwrap_or(false) { args.push(s_KEY); }
+        if show_options.option_value.unwrap_or(false) { args.push(v_KEY); }
+        if show_options.window_options.unwrap_or(false) { args.push(w_KEY); }
+        show_options.target.as_ref().and_then(|s| Some(args.extend_from_slice(&[t_KEY, &s])));
+        show_options.option.as_ref().and_then(|s| Some(args.push(&s)));
+        let output = self.subcommand(TmuxInterface::SHOW_OPTIONS, &args)?;
+        Ok(output.status.success())
     }
 
 
