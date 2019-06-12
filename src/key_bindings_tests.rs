a4323f0981c8c98ed36ac2c60d2bc415c5380357
Anton Gepting <anton.gepting@gmail.com>
2019-06-12 23:43:36 +0200

    test: remove cow type

--- src/key_bindings_tests.rs
+++ src/key_bindings_tests.rs
@@ -4,11 +4,10 @@
 fn send_keys() {
     use crate::tmux_interface::TmuxInterface;
     use crate::SendKeys;
-    use std::borrow::Cow;
 
     let tmux = TmuxInterface::new();
     let send_keys = SendKeys {
-        target_pane: Some(Cow::Borrowed("0:1.0")),
+        target_pane: Some("0:1.0"),
         keys: vec!["top", "C-m"],
         ..Default::default()
     };
