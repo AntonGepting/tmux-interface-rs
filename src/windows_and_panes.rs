6a9757ab2070e8075888558e3fdae23abf4a5750
Anton Gepting <anton.gepting@gmail.com>
2019-06-18 21:05:44 +0200

    feat: add resize-window, respawn-pane, -window functions

--- src/windows_and_panes.rs
+++ src/windows_and_panes.rs
@@ -310,6 +310,87 @@ impl<'a> NewWindow<'a> {
 }
 
 
+/// Resize a window, up, down, left or right
+///
+/// # Manual
+///
+/// ```text
+/// tmux resize-window [-aADLRU] [-t target-window] [-x width] [-y height] [adjustment]
+/// (alias: resizew)
+/// ```
+#[derive(Default)]
+pub struct ResizeWindow<'a> {
+    pub smallest: Option<bool>,                 // [-a]
+    pub largest: Option<bool>,                  // [-A]
+    pub down: Option<bool>,                     // [-D]
+    pub left: Option<bool>,                     // [-L]
+    pub right: Option<bool>,                    // [-R]
+    pub up: Option<bool>,                       // [-U]
+    pub target_window: Option<&'a str>,         // [-t target-window]
+    pub width: Option<usize>,                   // [-x width]
+    pub height: Option<usize>,                  // [-y height]
+    pub adjustment: Option<&'a str>,            // [adjustment]
+}
+
+
+impl<'a> ResizeWindow<'a> {
+    pub fn new() -> ResizeWindow<'a> {
+        Default::default()
+    }
+}
+
+
+/// Reactivate a pane in which the command has exited
+///
+/// # Manual
+///
+/// ```text
+/// tmux respawn-pane [-k] [-c start-directory] [-e environment] [-t target-pane] [shell-command]
+/// (alias: respawnp)
+/// ```
+#[derive(Default)]
+pub struct RespawnPane<'a> {
+    pub kill: Option<bool>,                     // [-k]
+    pub start_directory: Option<&'a str>,       // [-c start-directory]
+    pub environment: Option<&'a str>,           // [-e environment]
+    pub target_pane: Option<&'a str>,           // [-t target-pane]
+    pub shell_command: Option<&'a str>,         // [shell-command]
+}
+
+
+impl<'a> RespawnPane<'a> {
+    pub fn new() -> RespawnPane<'a> {
+        Default::default()
+    }
+}
+
+
+/// Reactivate a window in which the command has exited
+///
+/// # Manual
+///
+/// ```text
+/// tmux respawn-window [-k] [-c start-directory] [-e environment] [-t target-window]
+/// [shell-command]
+/// (alias: respawnw)
+/// ```
+#[derive(Default)]
+pub struct RespawnWindow<'a> {
+    pub kill: Option<bool>,                     // [-k]
+    pub start_directory: Option<&'a str>,       // [-c start-directory]
+    pub environment: Option<&'a str>,           // [-e environment]
+    pub target_window: Option<&'a str>,         // [-t target-pane]
+    pub shell_command: Option<&'a str>,         // [shell-command]
+}
+
+
+impl<'a> RespawnWindow<'a> {
+    pub fn new() -> RespawnWindow<'a> {
+        Default::default()
+    }
+}
+
+
 /// # Manual
 ///
 /// ```text
@@ -426,8 +507,9 @@ impl<'a> TmuxInterface<'a> {
     const PREVIOUS_WINDOW: &'static str = "previous-window";
     const RENAME_WINDOW: &'static str = "rename-window";
     const RESIZE_PANE: &'static str = "resize-pane";
-    //const RESIZE_WINDOW: &'static str = "resize-window";
-    //const RESPAWN_WINDOW: &'static str = "respawn-window";
+    const RESIZE_WINDOW: &'static str = "resize-window";
+    const RESPAWN_PANE: &'static str = "respawn-pane";
+    const RESPAWN_WINDOW: &'static str = "respawn-window";
     const ROTATE_WINDOW: &'static str = "rotate-window";
     //const SELECT_LAYOUT: &'static str = "select-layout";
     const SELECT_PANE: &'static str = "select-pane";
@@ -992,8 +1074,28 @@ impl<'a> TmuxInterface<'a> {
     /// tmux resize-window [-aADLRU] [-t target-window] [-x width] [-y height] [adjustment]
     /// (alias: resizew)
     /// ```
-    pub fn resize_window(&self) {
-        unimplemented!();
+    pub fn resize_window(&self, resize_window: &ResizeWindow) -> Result<bool, TmuxInterfaceError> {
+        let mut args: Vec<&str> = Vec::new();
+        if resize_window.smallest.unwrap_or(false) { args.push(a_KEY); }
+        if resize_window.largest.unwrap_or(false) { args.push(A_KEY); }
+        if resize_window.down.unwrap_or(false) { args.push(D_KEY); }
+        if resize_window.left.unwrap_or(false) { args.push(L_KEY); }
+        if resize_window.right.unwrap_or(false) { args.push(R_KEY); }
+        if resize_window.up.unwrap_or(false) { args.push(U_KEY); }
+        resize_window.target_window.and_then(|s| Some(args.extend_from_slice(&[t_KEY, &s])));
+        let x;
+        if let Some(width) = resize_window.width {
+            x = width.to_string();
+            args.extend_from_slice(&[x_KEY, &x]);
+        }
+        let y;
+        if let Some(height) = resize_window.height {
+            y = height.to_string();
+            args.extend_from_slice(&[y_KEY, &y]);
+        }
+        resize_window.adjustment.and_then(|s| Some(args.push(s)));
+        let output = self.subcommand(TmuxInterface::RESIZE_WINDOW, &args)?;
+        Ok(output.status.success())
     }
 
 
@@ -1005,8 +1107,15 @@ impl<'a> TmuxInterface<'a> {
     /// tmux respawn-pane [-k] [-c start-directory] [-e environment] [-t target-pane] [shell-command]
     /// (alias: respawnp)
     /// ```
-    pub fn respawn_pane(&self) {
-        unimplemented!();
+    pub fn respawn_pane(&self, respawn_pane: &RespawnPane) -> Result<bool, TmuxInterfaceError> {
+        let mut args: Vec<&str> = Vec::new();
+        if respawn_pane.kill.unwrap_or(false) { args.push(k_KEY); }
+        respawn_pane.start_directory.and_then(|s| Some(args.extend_from_slice(&[c_KEY, &s])));
+        respawn_pane.environment.and_then(|s| Some(args.extend_from_slice(&[e_KEY, &s])));
+        respawn_pane.target_pane.and_then(|s| Some(args.extend_from_slice(&[t_KEY, &s])));
+        respawn_pane.shell_command.and_then(|s| Some(args.push(s)));
+        let output = self.subcommand(TmuxInterface::RESPAWN_PANE, &args)?;
+        Ok(output.status.success())
     }
 
 
@@ -1019,8 +1128,15 @@ impl<'a> TmuxInterface<'a> {
     /// [shell-command]
     /// (alias: respawnw)
     /// ```
-    pub fn respawn_window(&self) {
-        unimplemented!();
+    pub fn respawn_window(&self, respawn_window: &RespawnWindow) -> Result<bool, TmuxInterfaceError> {
+        let mut args: Vec<&str> = Vec::new();
+        if respawn_window.kill.unwrap_or(false) { args.push(k_KEY); }
+        respawn_window.start_directory.and_then(|s| Some(args.extend_from_slice(&[c_KEY, &s])));
+        respawn_window.environment.and_then(|s| Some(args.extend_from_slice(&[e_KEY, &s])));
+        respawn_window.target_window.and_then(|s| Some(args.extend_from_slice(&[t_KEY, &s])));
+        respawn_window.shell_command.and_then(|s| Some(args.push(s)));
+        let output = self.subcommand(TmuxInterface::RESPAWN_WINDOW, &args)?;
+        Ok(output.status.success())
     }
 
 
