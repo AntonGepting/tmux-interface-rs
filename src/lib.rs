eb05af1786dceb26577df84571d08eb582641086
Anton Gepting <anton.gepting@gmail.com>
2019-06-21 07:37:01 +0200

    chore: reexport clients and sessions, windows and panes structs

--- src/lib.rs
+++ src/lib.rs
@@ -19,22 +19,46 @@ pub mod tmux_option;
 
 
 pub use self::tmux_interface::TmuxInterface;
-pub use self::clients_and_sessions::NewSession;
 pub use self::clients_and_sessions::AttachSession;
+pub use self::clients_and_sessions::DetachClient;
+pub use self::clients_and_sessions::NewSession;
+pub use self::clients_and_sessions::RefreshClient;
+pub use self::clients_and_sessions::SwitchClient;
+
+pub use self::windows_and_panes::BreakPane;
+pub use self::windows_and_panes::CapturePane;
+pub use self::windows_and_panes::ChooseClient;
+pub use self::windows_and_panes::ChooseTree;
+pub use self::windows_and_panes::FindWindow;
+pub use self::windows_and_panes::JoinPane;
+pub use self::windows_and_panes::LinkWindow;
+pub use self::windows_and_panes::MovePane;
+pub use self::windows_and_panes::MoveWindow;
 pub use self::windows_and_panes::NewWindow;
-pub use self::windows_and_panes::SplitWindow;
+pub use self::windows_and_panes::PipePane;
+pub use self::windows_and_panes::ResizePane;
+pub use self::windows_and_panes::ResizeWindow;
+pub use self::windows_and_panes::RespawnPane;
+pub use self::windows_and_panes::RespawnWindow;
+pub use self::windows_and_panes::SelectLayot;
 pub use self::windows_and_panes::SelectPane;
 pub use self::windows_and_panes::SelectWindow;
+pub use self::windows_and_panes::SplitWindow;
+pub use self::windows_and_panes::SwapPane;
+
 pub use self::key_bindings::SendKeys;
+
+pub use self::options::ShowOptions;
+pub use self::tmux_option::TmuxOption;
+
 pub use self::session::Session;
 pub use self::sessions::Sessions;
 pub use self::window::Window;
 pub use self::windows::Windows;
 pub use self::pane::Pane;
 pub use self::panes::Panes;
+
 pub use self::tmux_interface_error::TmuxInterfaceError;
-pub use self::options::ShowOptions;
-pub use self::tmux_option::TmuxOption;
 
 
 #[cfg(test)]
