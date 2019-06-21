43ffdd02d579df32ad0c291f9bee511dec74422b
Anton Gepting <anton.gepting@gmail.com>
2019-06-21 22:37:23 +0200

    feat: add options functions

--- src/options.rs
+++ src/options.rs
@@ -2,6 +2,60 @@ use super::tmux_interface::*;
 use super::tmux_interface_error::TmuxInterfaceError;
 
 
+/// # Manual
+///
+/// ```text
+/// tmux set-option [-aFgoqsuw] [-t target-session | target-window] option value
+/// (alias: set)
+/// ```
+#[derive(Default)]
+pub struct SetOption<'a> {
+    pub append: Option<bool>,                   // [-a]
+    pub format: Option<bool>,                   // [-F]
+    pub global: Option<bool>,                   // [-g]
+    pub not_overwrite: Option<bool>,            // [-o]
+    pub quiet: Option<bool>,                    // [-q]
+    pub server: Option<bool>,                   // [-s]
+    pub unset: Option<bool>,                    // [-u]
+    pub window: Option<bool>,                   // [-w]
+    pub target: Option<&'a str>,                // [-t target-session | target-window]
+    pub option: &'a str,                        // option
+    pub value: &'a str                          // value
+}
+
+impl<'a> SetOption<'a> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+}
+
+
+/// # Manual
+///
+/// ```text
+/// tmux set-window-option [-aFgoqu] [-t target-window] option value
+/// (alias: setw)
+/// ```
+#[derive(Default)]
+pub struct SetWindowOption<'a> {
+    pub append: Option<bool>,                   // [-a]
+    pub format: Option<bool>,                   // [-F]
+    pub global: Option<bool>,                   // [-g]
+    pub not_overwrite: Option<bool>,            // [-o]
+    pub quiet: Option<bool>,                    // [-q]
+    pub unset: Option<bool>,                    // [-u]
+    pub target_window: Option<&'a str>,         // [-t target-window]
+    pub option: &'a str,                        // option
+    pub value: &'a str                          // value
+}
+
+impl<'a> SetWindowOption<'a> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+}
+
+
 /// Show options structure
 ///
 /// # Manual
@@ -30,7 +84,6 @@ pub struct ShowOptions<'a> {
     pub option: Option<&'a str>,                // [option]
 }
 
-
 impl<'a> ShowOptions<'a> {
     pub fn new() -> Self {
         Default::default()
@@ -42,7 +95,10 @@ impl<'a> ShowOptions<'a> {
 impl<'a> TmuxInterface<'a> {
 
 
+    const SET_OPTION: &'static str = "set-option";
+    const SET_WINDOW_OPTION: &'static str = "set-window-option";
     const SHOW_OPTIONS: &'static str = "show-options";
+    const SHOW_WINDOW_OPTIONS: &'static str = "show-window-options";
 
 
     /// # Manual
@@ -51,8 +107,21 @@ impl<'a> TmuxInterface<'a> {
     /// tmux set-option [-aFgoqsuw] [-t target-session | target-window] option value
     /// (alias: set)
     /// ```
-    pub fn set_option() {
-        unimplemented!();
+    pub fn set_option(&self, set_option: &SetOption) -> Result<bool, TmuxInterfaceError> {
+        let mut args: Vec<&str> = Vec::new();
+        if set_option.append.unwrap_or(false) { args.push(a_KEY); }
+        if set_option.format.unwrap_or(false) { args.push(F_KEY); }
+        if set_option.global.unwrap_or(false) { args.push(g_KEY); }
+        if set_option.not_overwrite.unwrap_or(false) { args.push(o_KEY); }
+        if set_option.quiet.unwrap_or(false) { args.push(q_KEY); }
+        if set_option.server.unwrap_or(false) { args.push(s_KEY); }
+        if set_option.unset.unwrap_or(false) { args.push(u_KEY); }
+        if set_option.window.unwrap_or(false) { args.push(w_KEY); }
+        set_option.target.and_then(|s| Some(args.extend_from_slice(&[t_KEY, &s])));
+        args.push(set_option.option);
+        args.push(set_option.value);
+        let output = self.subcommand(TmuxInterface::SET_OPTION, &args)?;
+        Ok(output.status.success())
     }
 
 
@@ -62,8 +131,19 @@ impl<'a> TmuxInterface<'a> {
     /// tmux set-window-option [-aFgoqu] [-t target-window] option value
     /// (alias: setw)
     /// ```
-    pub fn set_window_option() {
-        unimplemented!();
+    pub fn set_window_option(&self, set_window_option: &SetWindowOption) -> Result<bool, TmuxInterfaceError> {
+        let mut args: Vec<&str> = Vec::new();
+        if set_window_option.append.unwrap_or(false) { args.push(a_KEY); }
+        if set_window_option.format.unwrap_or(false) { args.push(F_KEY); }
+        if set_window_option.global.unwrap_or(false) { args.push(g_KEY); }
+        if set_window_option.not_overwrite.unwrap_or(false) { args.push(o_KEY); }
+        if set_window_option.quiet.unwrap_or(false) { args.push(q_KEY); }
+        if set_window_option.unset.unwrap_or(false) { args.push(u_KEY); }
+        set_window_option.target_window.and_then(|s| Some(args.extend_from_slice(&[t_KEY, &s])));
+        args.push(set_window_option.option);
+        args.push(set_window_option.value);
+        let output = self.subcommand(TmuxInterface::SET_WINDOW_OPTION, &args)?;
+        Ok(output.status.success())
     }
 
 
@@ -95,8 +175,19 @@ impl<'a> TmuxInterface<'a> {
     /// tmux show-window-options [-gv] [-t target-window] [option]
     /// (alias: showw)
     /// ```
-    pub fn show_window_options() {
-        unimplemented!();
+    pub fn show_window_options(&self,
+                               global: Option<bool>,
+                               only_value: Option<bool>,
+                               target_window: Option<&str>,
+                               option: Option<&str>
+                               ) -> Result<bool, TmuxInterfaceError> {
+        let mut args: Vec<&str> = Vec::new();
+        if global.unwrap_or(false) { args.push(g_KEY); }
+        if only_value.unwrap_or(false) { args.push(v_KEY); }
+        target_window.and_then(|s| Some(args.extend_from_slice(&[t_KEY, &s])));
+        option.and_then(|s| Some(args.push(&s)));
+        let output = self.subcommand(TmuxInterface::SHOW_WINDOW_OPTIONS, &args)?;
+        Ok(output.status.success())
     }
 
 
