07713611a409816722969ce79bd9ae5bc15f81d5
Anton Gepting <anton.gepting@gmail.com>
2019-07-02 02:55:17 +0200

    test: add full structure comparsion

--- src/session_tests.rs
+++ src/session_tests.rs
@@ -1,8 +1,25 @@
 #[test]
 fn parse() {
     use crate::Session;
+    use std::time::Duration;
 
     let session = Session::parse(":1:1557947146:1557947146:1:1557947146::::0:$0:0:0:3,2,1:3").unwrap();
-    assert_eq!(session.name, Some("0".to_string()));
-    assert_eq!(session.id, Some(0));
+    let session_sample = Session {
+        alerts: None,
+        attached: Some(1),
+        activity: Some(Duration::from_millis(1557947146)),
+        created: Some(Duration::from_millis(1557947146)),
+        format: Some("1".to_string()),
+        last_attached: Some(Duration::from_millis(1557947146)),
+        group: None,
+        group_size: None,
+        group_list: None,
+        grouped: Some("0".to_string()),
+        id: Some(0),
+        many_attached: Some("0".to_string()),
+        name: Some("0".to_string()),
+        stack: Some("3,2,1".to_string()),
+        windows: Some(3),
+    };
+    assert_eq!(session, session_sample);
 }
