8760e8a3a2efb958acbb05a3564e98b0d117263e
Anton Gepting <anton.gepting@gmail.com>
2019-08-26 18:41:48 +0200

    test(error): add tmux error mock script

---
+++ tests/tmux_error_mock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# get subcommand
+subcommand=$1
+
+# process subcommand
+case $subcommand in
+    "-V")
+        # return wrong version
+        echo "tmux a2.9a\n"
+        ;;
+esac
