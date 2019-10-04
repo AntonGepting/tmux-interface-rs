b728bde35efc4b32cdc194cc0c8554f9fe0493c0
Anton Gepting <anton.gepting@gmail.com>
2019-10-04 08:04:31 +0200

    test(options): remove base-index test

--- src/tmux_option_tests.rs
+++ src/tmux_option_tests.rs
@@ -1,9 +1,10 @@
+// XXX: failed on default tmux settings?
 #[test]
 fn show_options_get_int() {
-    use crate::TmuxOption;
+    //use crate::TmuxOption;
 
-    let base_index = TmuxOption::get_int("base-index").unwrap();
-    assert_eq!(base_index, 1);
+    //let base_index = TmuxOption::get_int("base-index").unwrap();
+    //assert_eq!(base_index, 1);
 
     //let base_index = TmuxOption::get_int("pane-base-index").unwrap();
     //assert_eq!(base_index, 0);
