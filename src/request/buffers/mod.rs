67743709424c0670bc8bbdf75d85a84268f2f871
Anton Gepting <anton.gepting@gmail.com>
2020-01-17 23:54:50 +0100

    tests(request): include tests in modules

--- src/request/buffers/mod.rs
+++ src/request/buffers/mod.rs
@@ -9,3 +9,13 @@ pub mod paste_buffer;
 pub mod save_buffer;
 pub mod set_buffer;
 pub mod show_buffer;
+
+pub mod choose_buffer_tests;
+pub mod clear_history_tests;
+pub mod delete_buffer_tests;
+pub mod list_buffers_tests;
+pub mod load_buffer_tests;
+pub mod paste_buffer_tests;
+pub mod save_buffer_tests;
+pub mod set_buffer_tests;
+pub mod show_buffer_tests;
