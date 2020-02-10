2890ee4276772dff925bff261cb17158fc583d0c
Anton Gepting <anton.gepting@gmail.com>
2020-02-10 15:24:10 +0100

    tests(readme_examples): add builder examples

--- tests/readme_examples.rs
+++ tests/readme_examples.rs
@@ -1,19 +1,30 @@
 #[test]
 fn example1() {
-    use tmux_interface::{AttachSession, NewSession, TargetPane, TargetSession, TmuxInterface};
+    use tmux_interface::{
+        AttachSession, AttachSessionBuilder, NewSession, NewSessionBuilder, TargetPane,
+        TargetSession, TmuxInterface,
+    };
 
     let target_session = TargetSession::Raw("session_name");
     let mut tmux = TmuxInterface::new();
-    let new_session = NewSession {
+    let _new_session = NewSession {
         detached: Some(true),
         session_name: Some("session_name"),
         ..Default::default()
     };
+    let new_session = NewSessionBuilder::new()
+        .detached()
+        .session_name("session_name")
+        .build();
     tmux.new_session(Some(&new_session)).unwrap();
-    let attach_session = AttachSession {
+
+    let _attach_session = AttachSession {
         target_session: Some(&target_session),
         ..Default::default()
     };
+    let attach_session = AttachSessionBuilder::new()
+        .target_session(&target_session)
+        .build();
     tmux.send_keys::<TargetPane>(None, &vec!["exit", "C-m"])
         .unwrap();
     tmux.attach_session(Some(&attach_session)).unwrap();
