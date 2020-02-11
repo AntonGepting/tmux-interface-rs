d1127e57937f7b06968862e85ae0bd882660a4ff
Anton Gepting <anton.gepting@gmail.com>
2020-02-11 21:39:58 +0100

    feat(windows_and_panes): add builder pattern

--- src/request/windows_and_panes/swap_window.rs
+++ src/request/windows_and_panes/swap_window.rs
@@ -1,5 +1,6 @@
 use crate::error::Error;
 use crate::tmux_interface::*;
+use std::fmt::Display;
 use std::process::Output;
 
 impl<'a> TmuxInterface<'a> {
@@ -13,21 +14,25 @@ impl<'a> TmuxInterface<'a> {
     /// tmux swap-window [-d] [-s src-window] [-t dst-window]
     /// (alias: swapw)
     /// ```
-    pub fn swap_window(
+    pub fn swap_window<T: Display>(
         &mut self,
         detached: Option<bool>,
-        src_window: Option<&str>,
-        dst_window: Option<&str>,
+        src_window: Option<&'a T>,
+        dst_window: Option<&'a T>,
     ) -> Result<Output, Error> {
         let mut args: Vec<&str> = Vec::new();
+        let s;
+        let t;
         if detached.unwrap_or(false) {
             args.push(d_KEY);
         }
-        if let Some(s) = src_window {
+        if let Some(src_window) = src_window {
+            s = src_window.to_string();
             args.extend_from_slice(&[s_KEY, &s])
         }
-        if let Some(s) = dst_window {
-            args.extend_from_slice(&[t_KEY, &s])
+        if let Some(dst_window) = dst_window {
+            t = dst_window.to_string();
+            args.extend_from_slice(&[t_KEY, &t])
         }
         let output = self.subcommand(TmuxInterface::SWAP_WINDOW, &args)?;
         Ok(output)
--- src/request/windows_and_panes/swap_window_tests.rs
+++ src/request/windows_and_panes/swap_window_tests.rs
@@ -1,6 +1,6 @@
 #[test]
 fn swap_window() {
-    use crate::{Error, TmuxInterface};
+    use crate::{Error, TargetWindow, TmuxInterface};
 
     let mut tmux = TmuxInterface::new();
     tmux.pre_hook = Some(Box::new(|bin, options, subcmd| {
@@ -12,6 +12,10 @@ fn swap_window() {
         );
         Err(Error::new("hook"))
     }));
-    tmux.swap_window(Some(true), Some("1"), Some("2"))
-        .unwrap_err();
+    tmux.swap_window(
+        Some(true),
+        Some(&TargetWindow::Raw("1")),
+        Some(&TargetWindow::Raw("2")),
+    )
+    .unwrap_err();
 }
