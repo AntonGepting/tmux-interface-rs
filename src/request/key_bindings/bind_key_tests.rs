5cae3ef57ab3970368a412bc7bcf4d8130119856
Anton Gepting <anton.gepting@gmail.com>
2020-02-28 16:51:22 +0100

    tests(request): fix tests for tmux 3.1

--- src/request/key_bindings/bind_key_tests.rs
+++ src/request/key_bindings/bind_key_tests.rs
@@ -1,3 +1,39 @@
+#[cfg(not(feature = "tmux_2_6"))]
+#[test]
+fn bind_key() {
+    use crate::{BindKey, BindKeyBuilder, Error, TmuxInterface};
+
+    let mut tmux = TmuxInterface::new();
+    tmux.pre_hook = Some(Box::new(|bin, options, subcmd| {
+        // tmux bind-key [-nr] [-N note] [-T key-table] key command [arguments]
+        // (alias: bind)
+        assert_eq!(
+            format!(r#"{:?} {:?} {:?}"#, bin, options, subcmd),
+            r#""tmux" [] ["bind-key", "-n", "-r", "-N", "1", "-T", "2", "3", "4", "5"]"#
+        );
+        Err(Error::Hook)
+    }));
+
+    let bind_key = BindKey {
+        root: Some(true),
+        repeat: Some(true),
+        note: Some("1"),
+        key_table: Some("2"),
+        arguments: Some("5"),
+    };
+    tmux.bind_key(Some(&bind_key), "3", "4").unwrap_err();
+
+    let bind_key = BindKeyBuilder::new()
+        .root()
+        .repeat()
+        .note("1")
+        .key_table("2")
+        .arguments("5")
+        .build();
+    tmux.bind_key(Some(&bind_key), "3", "4").unwrap_err();
+}
+
+#[cfg(feature = "tmux_2_6")]
 #[test]
 fn bind_key() {
     use crate::{BindKey, BindKeyBuilder, Error, TmuxInterface};
--- src/request/key_bindings/list_keys_tests.rs
+++ src/request/key_bindings/list_keys_tests.rs
@@ -1,3 +1,22 @@
+#[cfg(not(feature = "tmux_2_6"))]
+#[test]
+fn list_keys() {
+    use crate::{Error, TmuxInterface};
+
+    let mut tmux = TmuxInterface::new();
+    tmux.pre_hook = Some(Box::new(|bin, options, subcmd| {
+        // tmux list-keys [-N] [-T key-table]
+        // (alias: lsk)
+        assert_eq!(
+            format!(r#"{:?} {:?} {:?}"#, bin, options, subcmd),
+            r#""tmux" [] ["list-keys", "-N", "-T", "1"]"#
+        );
+        Err(Error::Hook)
+    }));
+    tmux.list_keys(Some(true), Some("1")).unwrap_err();
+}
+
+#[cfg(feature = "tmux_2_6")]
 #[test]
 fn list_keys() {
     use crate::{Error, TmuxInterface};
