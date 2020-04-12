85048c4c590982c0ef20503409d1ff9fb022acd1
Anton Gepting <anton.gepting@gmail.com>
2020-04-12 20:28:35 +0200

    tests(buffers): add choose_buffer features

--- src/request/buffers/choose_buffer_tests.rs
+++ src/request/buffers/choose_buffer_tests.rs
@@ -1,75 +1,96 @@
-#[cfg(not(feature = "tmux_2_6"))]
 #[test]
 fn choose_buffer() {
     use crate::{ChooseBuffer, ChooseBufferBuilder, Error, TargetPane, TmuxInterface};
 
     let mut tmux = TmuxInterface::new();
     tmux.pre_hook = Some(Box::new(|bin, options, subcmd| {
+        // # Manual
+        //
+        // tmux X.X:
+        // ```text
         // tmux choose-buffer [-NZr] [-F format] [-f filter] [-O sort-order] [-t target-pane] [template]
-        assert_eq!(
-            format!(r#"{:?} {:?} {:?}"#, bin, options, subcmd),
-            r#""tmux" [] ["choose-buffer", "-N", "-Z", "-r", "-F", "1", "-f", "2", "-O", "3", "-t", "4", "5"]"#
-        );
-        Err(Error::Hook)
-    }));
-
-    let choose_buffer = ChooseBuffer {
-        no_preview: Some(true),
-        zoom: Some(true),
-        reverse_sort_order: Some(true),
-        format: Some("1"),
-        filter: Some("2"),
-        sort_order: Some("3"),
-        target_pane: Some(&TargetPane::Raw("4")),
-        template: Some("5"),
-    };
-    tmux.choose_buffer(Some(&choose_buffer)).unwrap_err();
-
-    let choose_buffer = ChooseBufferBuilder::new()
-        .no_preview()
-        .zoom()
-        .reverse_sort_order()
-        .format("1")
-        .filter("2")
-        .sort_order("3")
-        .target_pane(&TargetPane::Raw("4"))
-        .template("5")
-        .build();
-    tmux.choose_buffer(Some(&choose_buffer)).unwrap_err();
-}
-
-#[cfg(feature = "tmux_2_6")]
-#[test]
-fn choose_buffer() {
-    use crate::{ChooseBuffer, ChooseBufferBuilder, Error, TargetPane, TmuxInterface};
-
-    let mut tmux = TmuxInterface::new();
-    tmux.pre_hook = Some(Box::new(|bin, options, subcmd| {
+        // ```
+        //
+        // tmux ^3.1:
+        // ```text
+        // tmux choose-buffer [-NZr] [-F format] [-f filter] [-O sort-order] [-t target-pane] [template]
+        // ```
+        //
+        // tmux ^2.7:
+        // ```text
+        // tmux choose-buffer [-NZ] [-F format] [-f filter] [-O sort-order] [-t target-pane] [template]
+        // ```
+        //
+        // tmux ^2.6:
+        // ```text
         // tmux choose-buffer [-N] [-F format] [-f filter] [-O sort-order] [-t target-pane] [template]
-        assert_eq!(
-            format!(r#"{:?} {:?} {:?}"#, bin, options, subcmd),
-            r#""tmux" [] ["choose-buffer", "-N", "-F", "1", "-f", "2", "-O", "3", "-t", "4", "5"]"#
-        );
+        // ```
+        //
+        // tmux ^1.7:
+        // ```text
+        // tmux choose-buffer [-F format] [-t target-pane] [template]
+        // ```
+        //
+        // tmux ^1.3:
+        // ```text
+        // tmux choose-buffer [-t target-pane] [template]
+        // ```
+        let mut s = Vec::new();
+        let o: Vec<&str> = Vec::new();
+        s.push("choose-buffer");
+        #[cfg(feature = "tmux_2_6")]
+        s.push("-N");
+        #[cfg(feature = "tmux_2_7")]
+        s.push("-Z");
+        #[cfg(feature = "tmux_3_1")]
+        s.push("-r");
+        #[cfg(feature = "tmux_1_7")]
+        s.extend_from_slice(&["-F", "1"]);
+        #[cfg(feature = "tmux_2_6")]
+        s.extend_from_slice(&["-f", "2"]);
+        #[cfg(feature = "tmux_2_6")]
+        s.extend_from_slice(&["-O", "3"]);
+        s.extend_from_slice(&["-t", "4"]);
+        s.push("5");
+        assert_eq!(bin, "tmux");
+        assert_eq!(options, &o);
+        assert_eq!(subcmd, &s);
         Err(Error::Hook)
     }));
 
     let choose_buffer = ChooseBuffer {
+        #[cfg(feature = "tmux_2_6")]
         no_preview: Some(true),
+        #[cfg(feature = "tmux_2_7")]
+        zoom: Some(true),
+        #[cfg(feature = "tmux_3_1")]
+        reverse_sort_order: Some(true),
+        #[cfg(feature = "tmux_1_7")]
         format: Some("1"),
+        #[cfg(feature = "tmux_2_6")]
         filter: Some("2"),
+        #[cfg(feature = "tmux_2_6")]
         sort_order: Some("3"),
         target_pane: Some(&TargetPane::Raw("4")),
         template: Some("5"),
     };
     tmux.choose_buffer(Some(&choose_buffer)).unwrap_err();
 
-    let choose_buffer = ChooseBufferBuilder::new()
-        .no_preview()
-        .format("1")
-        .filter("2")
-        .sort_order("3")
-        .target_pane(&TargetPane::Raw("4"))
-        .template("5")
-        .build();
+    let mut builder = ChooseBufferBuilder::new();
+    #[cfg(feature = "tmux_2_6")]
+    builder.no_preview();
+    #[cfg(feature = "tmux_2_7")]
+    builder.zoom();
+    #[cfg(feature = "tmux_3_1")]
+    builder.reverse_sort_order();
+    #[cfg(feature = "tmux_1_7")]
+    builder.format("1");
+    #[cfg(feature = "tmux_2_6")]
+    builder.filter("2");
+    #[cfg(feature = "tmux_2_6")]
+    builder.sort_order("3");
+    builder.target_pane(&TargetPane::Raw("4"));
+    builder.template("5");
+    let choose_buffer = builder.build();
     tmux.choose_buffer(Some(&choose_buffer)).unwrap_err();
 }
