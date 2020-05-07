12eaf419c8741201d31c21d5b88965767279a11a
Anton Gepting <anton.gepting@gmail.com>
2020-05-07 23:02:33 +0200

    tests(Travis CI): add tmux versions matrix

---
+++ scripts/tmux_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# create sources dir, with parents
+mkdir -p ~/source
+# create build dir, with parents
+mkdir -p ~/build/tmux-$TMUX_VERSION
+
+# check if sources not exist? download it
+if [[ ! -d ~/source/tmux-$TMUX_VERSION ]]; then
+    git clone https://github.com/tmux/tmux.git ~/source/tmux-$TMUX_VERSION;
+fi
+
+cd ~/source/tmux-$TMUX_VERSION
+git checkout $TMUX_VERSION
+export PATH=$HOME/build/tmux-$TMUX_VERSION/bin:$PATH
+
+if [[ $TMUX_VERSION == "master" || ! $(tmux -V | grep $TMUX_VERSION) ]]; then
+    sh autogen.sh;
+fi
+
+if [[ $TMUX_VERSION == "master" || ! $(tmux -V | grep $TMUX_VERSION) ]]; then
+    ./configure --prefix=$HOME/build/tmux-$TMUX_VERSION && make && make install;
+fi
+
+cd $TRAVIS_BUILD_DIR
+
+tmux -V
