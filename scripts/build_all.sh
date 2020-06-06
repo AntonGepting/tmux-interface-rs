94f04a51a42a0b2e60f76b366b30f7b33c7dc4bd
Anton Gepting <anton.gepting@gmail.com>
2020-06-06 14:35:51 +0200

    tests(scripts): refactor, add var file, build script

---
+++ scripts/build_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# compilation root
+SRC_ROOT=".."
+# global variables file
+VARIABLES_FILE="versions.var"
+
+# import varibales
+source $VARIABLES_FILE
+
+
+cd $SRC_ROOT
+
+# iterate over versions
+for crate_feature in "${CRATE_FEATURES[@]}"
+do
+    echo
+    echo "Processing with feature $crate_feature ..."
+    echo
+
+    cargo build --features $crate_feature --no-default-features --verbose
+
+    echo
+    echo "Processing with feature $crate_feature ... done"
+    echo
+done
--- scripts/generate_manpages.sh
+++ scripts/generate_manpages.sh
@@ -9,18 +9,18 @@ SRC="tmux"
 DEST="../manuals"
 # remote sources url
 GIT_URL="https://github.com/tmux/tmux.git"
-VERSIONS=("0.8" "0.9" \
-    "1.0" "1.1" "1.2" "1.3" "1.4" "1.5" "1.6" "1.7" "1.8" "1.9" "1.9a" \
-    "2.0" "2.1" "2.2" "2.3" "2.4" "2.5" "2.6" "2.7" "2.8" "2.9" "2.9a" \
-    "3.0" "3.0a" "3.1" "3.1a" "3.1b" \
-    "master" )
+# global variables file
+VARIABLES_FILE="versions.var"
+
+# import varibales
+source $VARIABLES_FILE
 
 # download sources
 git clone $GIT_URL $SRC
 cd $SRC
 
 # iterate over versions
-for version in "${VERSIONS[@]}"
+for version in "${TMUX_VERSIONS[@]}"
 do
     echo
     echo "Processing tmux $version ..."
