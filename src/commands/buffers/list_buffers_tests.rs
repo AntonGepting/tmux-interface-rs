8a5259fbfe43b8ac0ae5e12d15d96a8330d053d8
Anton Gepting <anton.gepting@gmail.com>
2021-03-31 10:41:56 +0200

    chore: fix conditionals for tests

--- src/commands/buffers/list_buffers_tests.rs
+++ src/commands/buffers/list_buffers_tests.rs
@@ -35,19 +35,28 @@ fn list_buffers() {
     #[cfg(feature = "use_cmd_alias")]
     let cmd = "lsb";
 
-    #[cfg(all(not(feature = "tmux_1_5"), feature = "tmux_1_7"))]
+    #[cfg(any(
+        all(feature = "tmux_0_8", not(feature = "tmux_1_5")),
+        feature = "tmux_1_7"
+    ))]
     let mut s = Vec::new();
     #[cfg(feature = "tmux_1_7")]
     s.extend_from_slice(&["-F", "1"]);
     #[cfg(all(feature = "tmux_0_8", not(feature = "tmux_1_5")))]
     s.extend_from_slice(&["-t", "2"]);
-    #[cfg(all(not(feature = "tmux_1_5"), feature = "tmux_1_7"))]
+    #[cfg(any(
+        all(feature = "tmux_0_8", not(feature = "tmux_1_5")),
+        feature = "tmux_1_7"
+    ))]
     let s = s.into_iter().map(|a| a.into()).collect();
 
     assert_eq!(list_buffers.0.bin, Cow::Borrowed("tmux"));
     assert_eq!(list_buffers.0.bin_args, None);
     assert_eq!(list_buffers.0.cmd, Some(Cow::Borrowed(cmd)));
-    #[cfg(all(not(feature = "tmux_1_5"), feature = "tmux_1_7"))]
+    #[cfg(any(
+        all(feature = "tmux_0_8", not(feature = "tmux_1_5")),
+        feature = "tmux_1_7"
+    ))]
     assert_eq!(list_buffers.0.cmd_args, Some(s));
     #[cfg(all(feature = "tmux_1_5", not(feature = "tmux_1_7")))]
     assert_eq!(list_buffers.0.cmd_args, None);
