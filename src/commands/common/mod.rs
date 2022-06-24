42b2e513b86e472e238f278f8b377fc168fcd0fb
Anton Gepting <anton.gepting@gmail.com>
2022-06-24 16:06:03 +0200

    chore(commands): move position to common directory

--- src/commands/common/mod.rs
+++ src/commands/common/mod.rs
@@ -1,7 +1,13 @@
 #[cfg(feature = "tmux_2_9a")]
 pub mod client_flags;
 pub mod pane_size;
+#[cfg(feature = "tmux_3_2")]
+pub mod position;
 
 #[cfg(feature = "tmux_2_9a")]
 pub use self::client_flags::ClientFlags;
 pub use self::pane_size::PaneSize;
+#[cfg(feature = "tmux_3_2")]
+pub use self::position::PositionX;
+#[cfg(feature = "tmux_3_2")]
+pub use self::position::PositionY;
