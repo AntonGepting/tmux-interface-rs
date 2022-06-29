ca4fb5f93c42a5e874b1201a202ca9b5c35e3df9
Anton Gepting <anton.gepting@gmail.com>
2022-06-29 13:04:47 +0200

    feat(commands): add tmux_3_3 support
    for `display-popup` and it's unit test

--- src/commands/status_line/display_popup.rs
+++ src/commands/status_line/display_popup.rs
@@ -2,10 +2,56 @@ use crate::commands::common::Size;
 use crate::commands::constants::*;
 use crate::TmuxCommand;
 use std::borrow::Cow;
+#[cfg(feature = "tmux_3_3")]
+use std::fmt;
+
+/// Set the type of characters used for drawing popup borders. type may be one of:
+/// ‘double’ and ‘heavy’ will fall back to standard ACS line drawing when UTF-8 is not supported.
+#[cfg(feature = "tmux_3_3")]
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub enum PopupBorderLinesType {
+    /// single lines using ACS or UTF-8 characters (default)
+    Single,
+    /// variation of single with rounded corners using UTF-8 characters
+    Rounded,
+    /// double lines using UTF-8 characters
+    Double,
+    /// heavy lines using UTF-8 characters
+    Heavy,
+    /// simple ASCII characters
+    Simple,
+    /// simple ASCII space character
+    Padded,
+    /// no border
+    NoBorder,
+}
+
+#[cfg(feature = "tmux_3_3")]
+impl fmt::Display for PopupBorderLinesType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match self {
+            Self::Single => "single",
+            Self::Rounded => "rounded",
+            Self::Double => "double",
+            Self::Heavy => "heavy",
+            Self::Simple => "simple",
+            Self::Padded => "padded",
+            Self::NoBorder => "none",
+        };
+        write!(f, "{}", s)
+    }
+}
 
 /// Structure for displaying a menu on target-client
 ///
 /// # Manual
+///
+/// tmux ^3.3:
+/// ```text
+/// display-popup [-BCE] [-b border-lines] [-c target-client] [-d start-directory] [-e environment]
+/// [-h height] [-s style] [-S border-style] [-t target-pane] [-T title] [-w width] [-x position]
+/// [-y position] [shell-command]
+/// ```
 ///
 /// tmux ^3.2:
 /// ```text
@@ -16,8 +62,12 @@ use std::borrow::Cow;
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
 #[cfg(feature = "tmux_3_2")]
 pub struct DisplayPopup<'a> {
+    /// `[-B]` - not surround the popup by a border
+    #[cfg(feature = "tmux_3_3")]
+    pub no_border: bool,
+
     /// `[-C]` - closes any popup on the client
-    #[cfg(feature = "tmux_3_2")]
+    #[cfg(feature = "tmux_3_3")]
     pub close: bool,
 
     /// `[-E]` - closes the popup automatically when shell-command exits
@@ -28,6 +78,11 @@ pub struct DisplayPopup<'a> {
     #[cfg(feature = "tmux_3_2")]
     pub close_on_success: bool,
 
+    /// `[-b border-lines]` - sets the type of border line for the popup.  When -B is specified, the
+    /// -b option is ignored.  See popup-border-lines for possible values for border-lines
+    #[cfg(feature = "tmux_3_3")]
+    pub border_lines: Option<PopupBorderLinesType>,
+
     /// `[-c target-client]` - target-client
     #[cfg(feature = "tmux_3_2")]
     pub target_client: Option<Cow<'a, str>>,
@@ -36,13 +91,33 @@ pub struct DisplayPopup<'a> {
     #[cfg(feature = "tmux_3_2")]
     pub start_directory: Option<Cow<'a, str>>,
 
+    // XXX: struct instead of tuple
+    /// `[-e environment]` - takes the form ‘VARIABLE=value’ and sets an environment variable for
+    /// the popup; it may be specified multiple times
+    #[cfg(feature = "tmux_3_3")]
+    pub environment: Option<(Cow<'a, str>, Cow<'a, str>)>,
+
     /// `[-h height]` - height of the popup
     #[cfg(feature = "tmux_3_2")]
     pub height: Option<Size>,
 
+    // TODO: not imlemented!
+    /// `[-s style]` - sets the style for the popup
+    #[cfg(feature = "tmux_3_3")]
+    pub style: Option<Cow<'a, str>>,
+
+    // TODO: not imlemented!
+    /// `[-S border-style]` - sets the style for the popup border
+    #[cfg(feature = "tmux_3_3")]
+    pub border_style: Option<Cow<'a, str>>,
+
     /// `[-t target-pane]` - target-pane
     #[cfg(feature = "tmux_3_2")]
     pub target_pane: Option<Cow<'a, str>>,
+
+    /// `[-T title]` - is a format for the popup title
+    #[cfg(feature = "tmux_3_3")]
+    pub title: Option<Cow<'a, str>>,
 
     /// `[-w width]` - width of the popup
     #[cfg(feature = "tmux_3_2")]
@@ -66,6 +141,13 @@ impl<'a> DisplayPopup<'a> {
         Default::default()
     }
 
+    /// `[-B]` - not surround the popup by a border
+    #[cfg(feature = "tmux_3_3")]
+    pub fn no_border(mut self) -> Self {
+        self.no_border = true;
+        self
+    }
+
     /// `[-C]` - closes any popup on the client
     #[cfg(feature = "tmux_3_2")]
     pub fn close(mut self) -> Self {
@@ -87,6 +169,14 @@ impl<'a> DisplayPopup<'a> {
         self
     }
 
+    /// `[-b border-lines]` - sets the type of border line for the popup.  When -B is specified, the
+    /// -b option is ignored.  See popup-border-lines for possible values for border-lines
+    #[cfg(feature = "tmux_3_2")]
+    pub fn border_lines(mut self, border_lines: PopupBorderLinesType) -> Self {
+        self.border_lines = Some(border_lines);
+        self
+    }
+
     /// `[-c target-client]` - target-client
     #[cfg(feature = "tmux_3_2")]
     pub fn target_client<S: Into<Cow<'a, str>>>(mut self, target_client: S) -> Self {
@@ -101,6 +191,15 @@ impl<'a> DisplayPopup<'a> {
         self
     }
 
+    // TODO: vec
+    /// `[-e environment]` - takes the form ‘VARIABLE=value’ and sets an environment variable for
+    /// the popup; it may be specified multiple times
+    #[cfg(feature = "tmux_3_3")]
+    pub fn environment<S: Into<Cow<'a, str>>>(mut self, variable: S, value: S) -> Self {
+        self.environment = Some((variable.into(), value.into()));
+        self
+    }
+
     /// `[-h height]` - height of the popup
     #[cfg(feature = "tmux_3_2")]
     pub fn height(mut self, height: Size) -> Self {
@@ -108,10 +207,31 @@ impl<'a> DisplayPopup<'a> {
         self
     }
 
+    /// `[-s style]` - sets the style for the popup
+    #[cfg(feature = "tmux_3_3")]
+    pub fn style<S: Into<Cow<'a, str>>>(mut self, style: S) -> Self {
+        self.style = Some(style.into());
+        self
+    }
+
+    /// `[-S border-style]` - sets the style for the popup border
+    #[cfg(feature = "tmux_3_3")]
+    pub fn border_style<S: Into<Cow<'a, str>>>(mut self, border_style: S) -> Self {
+        self.border_style = Some(border_style.into());
+        self
+    }
+
     /// `[-t target-pane]` - target-pane
     #[cfg(feature = "tmux_3_2")]
     pub fn target_pane<S: Into<Cow<'a, str>>>(mut self, target_pane: S) -> Self {
         self.target_pane = Some(target_pane.into());
+        self
+    }
+
+    /// `[-T title]` - is a format for the popup title
+    #[cfg(feature = "tmux_3_3")]
+    pub fn title<S: Into<Cow<'a, str>>>(mut self, title: S) -> Self {
+        self.title = Some(title.into());
         self
     }
 
@@ -148,6 +268,12 @@ impl<'a> DisplayPopup<'a> {
 
         cmd.name(DISPLAY_POPUP);
 
+        // `[-B]` - not surround the popup by a border
+        #[cfg(feature = "tmux_3_3")]
+        if self.no_border {
+            cmd.push_flag(B_UPPERCASE_KEY);
+        }
+
         // `[-C]` - closes any popup on the client
         #[cfg(feature = "tmux_3_2")]
         if self.close {
@@ -166,6 +292,12 @@ impl<'a> DisplayPopup<'a> {
             cmd.push_flag(EE_UPPERCASE_KEY);
         }
 
+        // `[-b border-lines]` - sets the type of border line for the popup
+        #[cfg(feature = "tmux_3_3")]
+        if let Some(border_lines) = self.border_lines {
+            cmd.push_option(B_LOWERCASE_KEY, border_lines.to_string());
+        }
+
         // `[-c target-client]` - target-client
         #[cfg(feature = "tmux_3_2")]
         if let Some(target_client) = self.target_client {
@@ -178,16 +310,44 @@ impl<'a> DisplayPopup<'a> {
             cmd.push_option(D_LOWERCASE_KEY, start_directory);
         }
 
+        // `[-e environment]` - takes the form ‘VARIABLE=value’ and sets an environment variable for
+        // the popup; it may be specified multiple times
+        #[cfg(feature = "tmux_3_3")]
+        if let Some(environment) = self.environment {
+            cmd.push_option(
+                E_LOWERCASE_KEY,
+                format!("{}={}", environment.0, environment.1),
+            );
+        }
+
         // `[-h height]` - height of the popup
         #[cfg(feature = "tmux_3_2")]
         if let Some(height) = self.height {
             cmd.push_option(H_LOWERCASE_KEY, height.to_string());
         }
 
+        // `[-s style]` - sets the style for the popup
+        #[cfg(feature = "tmux_3_3")]
+        if let Some(style) = self.style {
+            cmd.push_option(S_LOWERCASE_KEY, style);
+        }
+
+        // `[-S border-style]` - sets the style for the popup border
+        #[cfg(feature = "tmux_3_3")]
+        if let Some(border_style) = self.border_style {
+            cmd.push_option(S_UPPERCASE_KEY, border_style);
+        }
+
         // `[-t target-pane]` - target-client
         #[cfg(feature = "tmux_3_2")]
         if let Some(target_pane) = self.target_pane {
             cmd.push_option(T_LOWERCASE_KEY, target_pane);
+        }
+
+        // `[-T title]` - is a format for the popup title
+        #[cfg(feature = "tmux_3_3")]
+        if let Some(title) = self.title {
+            cmd.push_option(T_UPPERCASE_KEY, title);
         }
 
         // `[-w width]` - width of the popup
--- src/commands/status_line/display_popup_tests.rs
+++ src/commands/status_line/display_popup_tests.rs
@@ -1,6 +1,7 @@
 #[test]
 fn display_popup() {
     use crate::commands::common::pane_size::Size;
+    use crate::commands::status_line::display_popup::PopupBorderLinesType;
     use crate::{DisplayPopup, TargetPane};
     use std::borrow::Cow;
 
@@ -8,37 +9,56 @@ fn display_popup() {
     //
     // # Manual
     //
+    // tmux ^3.3:
+    // ```text
+    // display-popup [-BCE] [-b border-lines] [-c target-client] [-d start-directory] [-e environment]
+    // [-h height] [-s style] [-S border-style] [-t target-pane] [-T title] [-w width] [-x position]
+    // [-y position] [shell-command]
+    // ```
+    //
     // tmux ^3.2:
     // ```text
     // display-popup [-CE] [-c target-client] [-d start-directory] [-h height] [-t target-pane]
     // [-w width] [-x position] [-y position] [shell-command]
     // (alias: popup)
     // ```
-    let target_pane = TargetPane::Raw("4").to_string();
+    let target_pane = TargetPane::Raw("8").to_string();
 
     let display_popup = DisplayPopup::new();
+    #[cfg(feature = "tmux_3_3")]
+    let display_popup = display_popup.no_border();
     #[cfg(feature = "tmux_3_2")]
     let display_popup = display_popup.close();
     #[cfg(feature = "tmux_3_2")]
     let display_popup = display_popup.close_on_exit();
     #[cfg(feature = "tmux_3_2")]
     let display_popup = display_popup.close_on_success();
+    #[cfg(feature = "tmux_3_3")]
+    let display_popup = display_popup.border_lines(PopupBorderLinesType::NoBorder);
     #[cfg(feature = "tmux_3_2")]
     let display_popup = display_popup.target_client("1");
     #[cfg(feature = "tmux_3_2")]
     let display_popup = display_popup.start_directory("2");
+    #[cfg(feature = "tmux_3_3")]
+    let display_popup = display_popup.environment("3", "4");
     #[cfg(feature = "tmux_3_2")]
-    let display_popup = display_popup.height(Size::Size(3));
+    let display_popup = display_popup.height(Size::Size(5));
+    #[cfg(feature = "tmux_3_3")]
+    let display_popup = display_popup.style("6");
+    #[cfg(feature = "tmux_3_3")]
+    let display_popup = display_popup.border_style("7");
     #[cfg(feature = "tmux_3_2")]
     let display_popup = display_popup.target_pane(&target_pane);
+    #[cfg(feature = "tmux_3_3")]
+    let display_popup = display_popup.title("9");
     #[cfg(feature = "tmux_3_2")]
-    let display_popup = display_popup.width(Size::Size(5));
+    let display_popup = display_popup.width(Size::Size(10));
     #[cfg(feature = "tmux_3_2")]
-    let display_popup = display_popup.x(Size::Size(6));
+    let display_popup = display_popup.x(Size::Size(11));
     #[cfg(feature = "tmux_3_2")]
-    let display_popup = display_popup.y(Size::Size(7));
+    let display_popup = display_popup.y(Size::Size(12));
     #[cfg(feature = "tmux_3_2")]
-    let display_popup = display_popup.shell_command("8");
+    let display_popup = display_popup.shell_command("13");
 
     #[cfg(not(feature = "cmd_alias"))]
     let cmd = "display-popup";
@@ -47,28 +67,40 @@ fn display_popup() {
 
     let mut s = Vec::new();
     s.push(cmd);
+    #[cfg(feature = "tmux_3_3")]
+    s.push("-B");
     #[cfg(feature = "tmux_3_2")]
     s.push("-C");
     #[cfg(feature = "tmux_3_2")]
     s.push("-E");
     #[cfg(feature = "tmux_3_2")]
     s.push("-EE");
+    #[cfg(feature = "tmux_3_3")]
+    s.extend_from_slice(&["-b", "none"]);
     #[cfg(feature = "tmux_3_2")]
     s.extend_from_slice(&["-c", "1"]);
     #[cfg(feature = "tmux_3_2")]
     s.extend_from_slice(&["-d", "2"]);
+    #[cfg(feature = "tmux_3_3")]
+    s.extend_from_slice(&["-e", "3=4"]);
     #[cfg(feature = "tmux_3_2")]
-    s.extend_from_slice(&["-h", "3"]);
+    s.extend_from_slice(&["-h", "5"]);
+    #[cfg(feature = "tmux_3_3")]
+    s.extend_from_slice(&["-s", "6"]);
+    #[cfg(feature = "tmux_3_3")]
+    s.extend_from_slice(&["-S", "7"]);
     #[cfg(feature = "tmux_3_2")]
-    s.extend_from_slice(&["-t", "4"]);
+    s.extend_from_slice(&["-t", "8"]);
+    #[cfg(feature = "tmux_3_3")]
+    s.extend_from_slice(&["-T", "9"]);
     #[cfg(feature = "tmux_3_2")]
-    s.extend_from_slice(&["-w", "5"]);
+    s.extend_from_slice(&["-w", "10"]);
     #[cfg(feature = "tmux_3_2")]
-    s.extend_from_slice(&["-x", "6"]);
+    s.extend_from_slice(&["-x", "11"]);
     #[cfg(feature = "tmux_3_2")]
-    s.extend_from_slice(&["-y", "7"]);
+    s.extend_from_slice(&["-y", "12"]);
     #[cfg(feature = "tmux_3_2")]
-    s.push("8");
+    s.push("13");
     let s: Vec<Cow<str>> = s.into_iter().map(|a| a.into()).collect();
 
     let display_popup = display_popup.build().to_vec();
