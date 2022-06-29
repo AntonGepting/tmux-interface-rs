85d5041c3effd02b2e1e71f4503dbe094317132d
Anton Gepting <anton.gepting@gmail.com>
2022-06-29 13:03:56 +0200

    feat(commands): add show-prompt-history command

---
+++ src/commands/status_line/show_prompt_history.rs
@@ -0,0 +1,44 @@
+use crate::commands::constants::*;
+use crate::TmuxCommand;
+use std::borrow::Cow;
+
+/// # Manual
+///
+/// tmux ^3.3:
+/// ```text
+/// show-prompt-history [-T prompt-type]
+/// (alias: showhist)
+/// ```
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
+pub struct ShowPromptHistory<'a> {
+    /// `[-T prompt-type]`
+    #[cfg(feature = "tmux_3_3")]
+    pub prompt_type: Option<Cow<'a, str>>,
+}
+
+impl<'a> ShowPromptHistory<'a> {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    /// `[-T prompt]`
+    #[cfg(feature = "tmux_1_5")]
+    pub fn prompt_type<S: Into<Cow<'a, str>>>(mut self, prompt_type: S) -> Self {
+        self.prompt_type = Some(prompt_type.into());
+        self
+    }
+
+    pub fn build(self) -> TmuxCommand<'a> {
+        let mut cmd = TmuxCommand::new();
+
+        cmd.name(SHOW_PROMPT_HISTORY);
+
+        // `[-T prompt-type]`
+        #[cfg(feature = "tmux_1_5")]
+        if let Some(prompt_type) = self.prompt_type {
+            cmd.push_option(T_UPPERCASE_KEY, prompt_type);
+        }
+
+        cmd
+    }
+}
---
+++ src/commands/status_line/show_prompt_history_tests.rs
@@ -0,0 +1,31 @@
+#[test]
+fn clear_prompt_history() {
+    use crate::ClearPromptHistory;
+    use std::borrow::Cow;
+
+    // # Manual
+    //
+    // tmux ^3.3:
+    // ```text
+    // clear-prompt-history [-T prompt-type]
+    // (alias: clearphist)
+    // ```
+    let clear_prompt_history = ClearPromptHistory::new();
+    #[cfg(feature = "tmux_3_3")]
+    let clear_prompt_history = clear_prompt_history.prompt_type("1");
+
+    #[cfg(not(feature = "cmd_alias"))]
+    let cmd = "clear-prompt-history";
+    #[cfg(feature = "cmd_alias")]
+    let cmd = "clearphist";
+
+    let mut s = Vec::new();
+    s.push(cmd);
+    #[cfg(feature = "tmux_1_5")]
+    s.extend_from_slice(&["-T", "1"]);
+    let s: Vec<Cow<str>> = s.into_iter().map(|a| a.into()).collect();
+
+    let clear_prompt_history = clear_prompt_history.build().to_vec();
+
+    assert_eq!(clear_prompt_history, s);
+}
