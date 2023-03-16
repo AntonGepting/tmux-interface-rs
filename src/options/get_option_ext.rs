d68fc07aebb8ccd8ba045e02e0c0e08b455bcca3
Anton Gepting <anton.gepting@gmail.com>
2023-03-16 10:00:11 +0100

    chore(options): clean up, add doc, comments

--- src/options/get_option_ext.rs
+++ src/options/get_option_ext.rs
@@ -1,9 +1,28 @@
 use crate::{ShowOptions, TmuxCommand};
 use std::borrow::Cow;
 
-/// common trait for getting options, allowing different implementations for different object options
+/// Common trait for getting options, using [`ShowOptions`] command.
+///
+// Allows different implementations for different objects options:
+//
+//  * [`ServerOptions`] (`show-options -s <NAME>`)
+//  * [`SessionOptions`] (`show-options <NAME>`)
+//  * [`WindowOptions`] (`show-options -w <NAME>`)
+//  * [`PaneOptions`] (`show-options -p <NAME>`)
+//
+// NOTE: `GetOptionExt` complementary to `SetOptionExt`, symbols `SetOption`
+// already taken by tmux command
 pub trait GetOptionExt {
-    fn get<'a, T: Into<Cow<'a, str>>>(name: T) -> TmuxCommand<'a> {
-        ShowOptions::new().option(name).value().build()
+    fn get<'a, T, S>(target: Option<S>, name: T) -> TmuxCommand<'a>
+    where
+        T: Into<Cow<'a, str>>,
+        S: Into<Cow<'a, str>>,
+    {
+        let cmd = ShowOptions::new().option(name);
+        let cmd = match target {
+            Some(target) => cmd.target(target),
+            None => cmd,
+        };
+        cmd.build()
     }
 }
--- src/options/set_option_ext.rs
+++ src/options/set_option_ext.rs
@@ -1,79 +1,108 @@
 use crate::{SetOption, TmuxCommand, TmuxCommands};
 use std::borrow::Cow;
-use std::fmt;
 
+/// Common trait for setting options, using [`SetOption`] command.
+///
+// Allows different implementations for different object options:
+//  * [`ServerOptions`] (`set-option -s <NAME>`)
+//  * [`SessionOptions`] (`set-option <NAME>`)
+//  * [`WindowOptions`] (`set-option -w <NAME>`)
+//  * [`PaneOptions`] (`set-option -p <NAME>`)
+//
 // TODO: optimize set/set_ext are the same
-/// common trait for setting options, allowing different implementations for different object options
+// NOTE: `SetOptionExt`, symbols `SetOption` already taken by tmux command
 pub trait SetOptionExt {
-    fn set<'a, T: Into<Cow<'a, str>>, S: Into<Cow<'a, str>>>(
-        name: T,
-        value: Option<S>,
-    ) -> TmuxCommand<'a> {
-        match value {
-            Some(data) => Self::set_ext(name, Some(data)),
-            None => Self::unset(name),
-        }
-    }
-
-    fn unset<'a, T: Into<Cow<'a, str>>>(name: T) -> TmuxCommand<'a> {
-        SetOption::new().option(name).unset().build()
-    }
-
-    // unset if value = None
-    fn set_ext<'a, T: Into<Cow<'a, str>>, S: Into<Cow<'a, str>>>(
-        name: T,
-        value: Option<S>,
-    ) -> TmuxCommand<'a> {
-        //(self.setter)(name.into(), value.map(|s| s.into()))
+    // XXX: unset if value = None, mb merge set_ext?
+    //
+    /// set:
+    /// ```text
+    /// set-option [-t target] <NAME> <VALUE>
+    /// ```
+    ///
+    /// unset:
+    /// ```text
+    /// set-option [-t target] -u <NAME>
+    /// ```
+    fn set<'a, U, T, S>(target: Option<U>, name: T, value: Option<S>) -> TmuxCommand<'a>
+    where
+        U: Into<Cow<'a, str>>,
+        T: Into<Cow<'a, str>>,
+        S: Into<Cow<'a, str>>,
+    {
+        let cmd = SetOption::new().option(name);
+        let cmd = match target {
+            Some(target) => cmd.target(target),
+            None => cmd,
+        };
         let cmd = match value {
-            Some(data) => SetOption::new().option(name).value(data),
-            None => SetOption::new().option(name),
+            Some(value) => cmd.value(value),
+            None => cmd.unset(),
         };
         cmd.build()
     }
 
-    fn set_array<'a, S, I, T>(name: S, value: Option<I>) -> TmuxCommands<'a>
+    /// unset:
+    /// ```text
+    /// set-option [-t target] -u <NAME>
+    /// ```
+    fn unset<'a, S, T>(target: Option<S>, name: T) -> TmuxCommand<'a>
     where
         S: Into<Cow<'a, str>>,
-        I: IntoIterator<Item = T>,
         T: Into<Cow<'a, str>>,
     {
+        let cmd = SetOption::new().option(name).unset();
+        let cmd = match target {
+            Some(target) => cmd.target(target),
+            None => cmd,
+        };
+        cmd.build()
+    }
+
+    // fn set_ext<'a, U: Into<Cow<'a, str>>, T: Into<Cow<'a, str>>, S: Into<Cow<'a, str>>>(
+    //     target: Option<U>,
+    //     name: T,
+    //     value: Option<S>,
+    // ) -> TmuxCommand<'a> {
+    //     let cmd = SetOption::new().option(name);
+    //     let cmd = match target {
+    //         Some(target) => cmd.target(target),
+    //         None => cmd,
+    //     };
+    //     let cmd = match value {
+    //         Some(data) => cmd.value(data),
+    //         None => cmd,
+    //     };
+    //     cmd.build()
+    // }
+
+    ///
+    /// ```text
+    /// set-option [-t target] <NAME>[0] <VALUE>
+    /// set-option [-t target] <NAME>[1] <VALUE>
+    /// set-option [-t target] <NAME>[2] <VALUE>
+    /// set-option [-t target] <NAME>[...] <VALUE>
+    /// ```
+    ///
+    /// ```text
+    /// set-option [-t target] <NAME> ""
+    /// ```
+    ///
+    // XXX: check value None branch tmux reaction
+    fn set_array<'a, U, S, I, T>(target: Option<U>, name: S, value: Option<I>) -> TmuxCommands<'a>
+    where
+        S: Into<Cow<'a, str>> + std::fmt::Display,
+        I: IntoIterator<Item = T>,
+        T: Into<Cow<'a, str>>,
+        U: Into<Cow<'a, str>> + Copy,
+    {
         let mut cmds = TmuxCommands::new();
-        let name = name.into();
-        if let Some(data) = value {
-            for (i, item) in data.into_iter().enumerate() {
-                cmds.push(Self::set(format!("{}[{}]", name, i), Some(item.into())));
+        if let Some(value) = value {
+            for (i, item) in value.into_iter().enumerate() {
+                cmds.push(Self::set(target, format!("{}[{}]", name, i), Some(item)));
             }
         } else {
-            cmds.push(Self::set(format!("{}", name), Some("")));
+            cmds.push(Self::set(target, format!("{}", name), Some("")));
         }
         cmds
     }
-
-    fn set_array_original<'a, S: fmt::Display>(
-        name: S,
-        value: Option<Vec<String>>,
-    ) -> TmuxCommands<'a> {
-        let mut cmds = TmuxCommands::new();
-        if let Some(data) = value {
-            for (i, item) in data.iter().enumerate() {
-                cmds.push(Self::set(format!("{}[{}]", name, i), Some(item.to_owned())));
-            }
-        } else {
-            cmds.push(Self::set(format!("{}", name), Some("")));
-        }
-        cmds
-    }
-
-    // ### Manual
-    //
-    // ```text
-    // user option
-    // ```
-    //fn user_option<'a, S: Into<Cow<'a, str>>, T: Into<Cow<'a, str>>>(
-    //name: S,
-    //value: Option<T>,
-    //) -> TmuxCommand<'a> {
-    //Self::set(format!("{}{}", USER_OPTION_MARKER, name.into()), value)
-    //}
 }
