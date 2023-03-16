533ab8af85da96cce7112230147b08ab3754ca4d
Anton Gepting <anton.gepting@gmail.com>
2023-03-16 10:00:54 +0100

    tests(options): add get/set option trait tests

---
+++ src/options/get_option_ext_tests.rs
@@ -0,0 +1,22 @@
+use crate::GetOptionExt;
+
+struct GetOptionExtTest;
+
+impl GetOptionExt for GetOptionExtTest {}
+
+#[test]
+fn get_option_ext_tests() {
+    let target = ":";
+    let name = "option-name";
+
+    #[cfg(not(feature = "cmd_alias"))]
+    let cmd = "show-options";
+    #[cfg(feature = "cmd_alias")]
+    let cmd = "show";
+
+    let s = GetOptionExtTest::get(Some(target), name).to_string();
+
+    let origin = format!("{} -t {} {}", cmd, target, name);
+
+    assert_eq!(origin, s);
+}
---
+++ src/options/set_option_ext_tests.rs
@@ -0,0 +1,23 @@
+use crate::SetOptionExt;
+
+struct SetOptionExtTest;
+
+impl SetOptionExt for SetOptionExtTest {}
+
+#[test]
+fn set_option_ext_tests() {
+    let target = ":";
+    let name = "option-name";
+    let value = "value";
+
+    #[cfg(not(feature = "cmd_alias"))]
+    let cmd = "set-option";
+    #[cfg(feature = "cmd_alias")]
+    let cmd = "set";
+
+    let s = SetOptionExtTest::set(Some(target), name, Some(value)).to_string();
+
+    let origin = format!("{} -t {} {} {}", cmd, target, name, value);
+
+    assert_eq!(origin, s);
+}
