dca159e3bdb37d9160fb6a0fd97465fdd088c28c
Anton Gepting <anton.gepting@gmail.com>
2023-07-16 19:03:09 +0200

    docs(commands): fix warning

--- src/commands/clients_and_sessions/server_access.rs
+++ src/commands/clients_and_sessions/server_access.rs
@@ -28,7 +28,7 @@ pub struct ServerAccess<'a> {
     /// `[-w]` - write permission for user
     #[cfg(feature = "tmux_3_3")]
     pub write: bool,
-    /// `[user]` - user
+    /// `\[user\]` - user
     #[cfg(feature = "tmux_3_3")]
     pub user: Option<Cow<'a, str>>,
 }
@@ -38,42 +38,42 @@ impl<'a> ServerAccess<'a> {
         Default::default()
     }
 
-    /// [-a] - give access
+    /// `[-a]` - give access
     #[cfg(feature = "tmux_3_3")]
     pub fn add(mut self) -> Self {
         self.add = true;
         self
     }
 
-    /// [-d] - revoke access
+    /// `[-d]` - revoke access
     #[cfg(feature = "tmux_3_3")]
     pub fn delete(mut self) -> Self {
         self.delete = true;
         self
     }
 
-    /// [-l] - list current access permissions
+    /// `[-l]` - list current access permissions
     #[cfg(feature = "tmux_3_3")]
     pub fn list(mut self) -> Self {
         self.list = true;
         self
     }
 
-    /// [-r] - read-only permission for user
+    /// `[-r]` - read-only permission for user
     #[cfg(feature = "tmux_3_3")]
     pub fn read(mut self) -> Self {
         self.read = true;
         self
     }
 
-    /// [-w] - write permission for user
+    /// `[-w]` - write permission for user
     #[cfg(feature = "tmux_3_3")]
     pub fn write(mut self) -> Self {
         self.write = true;
         self
     }
 
-    /// [user] - user
+    /// `\[user\]` - user
     #[cfg(feature = "tmux_1_9")]
     pub fn user<S: Into<Cow<'a, str>>>(mut self, user: S) -> Self {
         self.user = Some(user.into());
@@ -85,37 +85,37 @@ impl<'a> ServerAccess<'a> {
 
         cmd.name(SERVER_ACCESS);
 
-        // [-a] - give access
+        // `[-a]` - give access
         #[cfg(feature = "tmux_3_3")]
         if self.add {
             cmd.push_flag(A_LOWERCASE_KEY);
         }
 
-        // [-d] - revoke access
+        // `[-d]` - revoke access
         #[cfg(feature = "tmux_3_3")]
         if self.delete {
             cmd.push_flag(D_LOWERCASE_KEY);
         }
 
-        // [-l] - list current access permissions
+        // `[-l]` - list current access permissions
         #[cfg(feature = "tmux_3_3")]
         if self.list {
             cmd.push_flag(L_LOWERCASE_KEY);
         }
 
-        // [-r] - read-only permission for user
+        // `[-r]` - read-only permission for user
         #[cfg(feature = "tmux_3_3")]
         if self.read {
             cmd.push_flag(R_LOWERCASE_KEY);
         }
 
-        // [-w] - write permission for user
+        // `[-w]` - write permission for user
         #[cfg(feature = "tmux_3_3")]
         if self.write {
             cmd.push_flag(W_LOWERCASE_KEY);
         }
 
-        // [user] - user
+        // `\[user\]` - user
         #[cfg(feature = "tmux_3_3")]
         if let Some(user) = self.user {
             cmd.push_param(user);
